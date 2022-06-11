6d3da379f428a298945ec9a9112b4b50bbbfe84c
Cyril Isip <cisip@Cyrils-MacBook-Pro.local>
2022-06-11 01:15:02 -0500

    fix heroku bug on momentjs

--- client/src/Components/Hour/Hour.js
+++ client/src/Components/Hour/Hour.js
@@ -1,7 +1,6 @@
 import React, { useContext } from 'react'
 import './Hour.css'
 import cloud from '../../utils/cloud.svg'
-import Moment from 'react-moment';
 import moment from 'moment-timezone';
 import ApiContext from '../ApiContext'
 function Hour({ temp, weather, dt, today, timezone, padding, className }) {
@@ -11,22 +10,25 @@ function Hour({ temp, weather, dt, today, timezone, padding, className }) {
   } = useContext(ApiContext)
   const currentDt = moment.unix(dt).utc().tz(timezone).format('ha')
   return (
-    <>
-      <div className={className} style={
-        padding
-      }>
-        <div>
-          {today ? <div>NOW</div> : <div>{currentDt}</div>}
-        </div>
+    <>{
+      isLoading ? <div></div> : <>
+        < div className={className} style={
+          padding
+        } >
+          <div>
+            {today ? <div>NOW</div> : <div>{currentDt}</div>}
+          </div>
 
-        <div className="HourImg md8">
-          {isLoading ?
-            <img className='HourImgSvg' src={cloud} alt="react" /> :
-            <img className='HourImgSvg' src={weatherx} alt="react" />
-          }
-        </div>
-        <div className='md8'>{Math.floor(temp)}°</div>
-      </div>
+          <div className="HourImg md8">
+            {isLoading ?
+              <img className='HourImgSvg' src={cloud} alt="react" /> :
+              <img className='HourImgSvg' src={weatherx} alt="react" />
+            }
+          </div>
+          <div className='md8'>{Math.floor(temp)}°</div>
+        </div >
+      </>
+    }
     </>
   )
 }
