0f1ced26ab2a8aadf35516641d98901a56a8bcaa
Cyril Isip <cisip@Cyrils-MacBook-Pro.local>
2022-06-11 02:18:49 -0500

    6

--- client/src/Components/Hour/Hour.js
+++ client/src/Components/Hour/Hour.js
@@ -3,25 +3,20 @@ import './Hour.css'
 import cloud from '../../utils/cloud.svg'
 import moment from 'moment-timezone';
 import ApiContext from '../ApiContext'
-function Hour({ temp, weather, dt, today, timezone, padding, className }) {
+function Hour({ temp, weather, dt, today, timezone, className }) {
 
   const weatherx = ` http://openweathermap.org/img/wn/${weather}@4x.png`
   const { isLoading,
   } = useContext(ApiContext)
-  const currentDt = moment.unix(dt)?.utc(timezone).format('ha')
+  const currentDt = moment.unix(dt).utc(timezone).format('ha')
   console.log(currentDt)
-  console.log(dt)
-  console.log(timezone)
-  // const currentDt = '6am'
   return (
-    <>{
-      isLoading ? <div>HELLO</div> : <>
-        < div className={className} style={
-          padding
-        } >
+    <>
+      <>
+        <div className={className}>
           <div>
             {today ? <div>NOW</div> : <div>
-              {currentDt ? <div> HELLO</div> : <div> {currentDt}</div>}
+              {currentDt ? <div> {currentDt}</div> : <div> HELLO</div>}
             </div>}
           </div>
 
@@ -34,9 +29,32 @@ function Hour({ temp, weather, dt, today, timezone, padding, className }) {
           <div className='md8'>{Math.floor(temp)}°</div>
         </div >
       </>
-    }
     </>
   )
 }
 
 export default Hour
+
+
+  // <> {
+  // isLoading?<div>HELLO</div> : <>
+  //   < div className={className} style={
+  //     padding
+  //   } >
+  //     <div>
+  //       {today ? <div>NOW</div> : <div>
+  //         {currentDt ? <div> HELLO</div> : <div> {currentDt}</div>}
+  //       </div>}
+  //     </div>
+
+  //     <div className="HourImg md8">
+  //       {isLoading ?
+  //         <img className='HourImgSvg' src={cloud} alt="react" /> :
+  //         <img className='HourImgSvg' src={weatherx} alt="react" />
+  //       }
+  //     </div>
+  //     <div className='md8'>{Math.floor(temp)}°</div>
+  //   </div >
+  // </>
+  //   }
+  //   </>
