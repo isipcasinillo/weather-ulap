728e295d99e5c608354332620e566f2418e7a723
isipcasinillo <isipcasinillo@gmail.com>
2022-07-28 18:28:53 -0500

    Update Rain.js

--- client/src/Components/Rain/Rain.js
+++ client/src/Components/Rain/Rain.js
@@ -8,7 +8,7 @@ function Rain() {
         <div className="Rain">
             <div className='RainContainer p8 cw'>
                 <div className='pdb16 p12'>RAIN</div>
-                <div className='pdb8 p32'>{arrayData[0]?.pop * 100}%</div>
+                <div className='pdb8 p32'>{Math.round(arrayData[0]?.pop * 100)}%</div>
                 <div className='pb60 p16'>Chance of rain</div>
                 <div className='pb16 p10'>{hourlyData[0]?.weather[0].description}</div>
             </div>
